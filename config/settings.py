04a93feedf7e1e5baeabb578dbb86eb49ccf6c71
rapsealk <piono623@naver.com>
2022-09-12 18:43:09 +0900

    feat(models): Add `Problem` and `Submission` models

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "onlinejudge.problems.apps.ProblemsConfig",
 ]
 
 MIDDLEWARE = [
